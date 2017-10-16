cbd750e39b7fe3cbb7494a562b61e150da0f49f3
mayazure <mayazureee@gmail.com>
2017-10-17 10:45:44 +1100

    Add data.

--- Sources/generator_controllers.adb
+++ Sources/generator_controllers.adb
@@ -18,20 +18,24 @@ package body Generator_Controllers is
 
    -- Parameters about the sine wave.
    Signal_Frequency : constant Integer := 1;
-   Sample_Frequency : constant Integer := 4;
+   Sample_Frequency : constant Integer := 16;
 
    -- Constants.
    PI : constant Float := 3.1415;
    PI_D : constant Float := PI * 2.0;
-   Sine_Omega : constant Float := PI_D * Float (Signal_Frequency);
-   type Sine_Taylor_Level is range 1 .. 5;
-   Sine_Taylor_Parameters : constant array (Sine_Taylor_Level) of Float := (-0.1666666667,
-                                                                            0.0083333333,
-                                                                            -0.0001984127,
-                                                                            2.75573192239859E-06,
-                                                                            -2.50521083854417E-08);
+   type Sine_Taylor_Level is range 1 .. 9;
+   Sine_Taylor_Parameters : constant array (Sine_Taylor_Level) of Float := (-1.6666666666667E-01,
+                                                                            8.3333333333333E-03,
+                                                                            -1.9841269841270E-04,
+                                                                            2.7557319223986E-06,
+                                                                            -2.5052108385442E-08,
+                                                                            1.6059043836822E-10,
+                                                                            -7.6471637318198E-13,
+                                                                            2.8114572543455E-15,
+                                                                            -8.2206352466243E-18);
+
    -- Sample parameters.
-   Sample_X_Slide : constant Float := 1000.0 / Float (Sample_Frequency);
+   Sample_X_Slide : constant Float := 1.0 / Float (Sample_Frequency);
    type Sample_Range is range 1 .. Sample_Frequency;
 
    -- Signal parameters.
@@ -55,7 +59,7 @@ package body Generator_Controllers is
    end Sine_Level;
 
    function Sine (X : Float) return Float is
-      Real_X : constant Float := Sine_Omega * X;
+      Real_X : constant Float := PI_D * X;
    begin
       return Real_X + Sine_Level (Base_X => Real_X,
                                   X_Square => Real_X * Real_X,
@@ -72,25 +76,34 @@ package body Generator_Controllers is
       Next_Sine_Start : Time := System_Start + Sine_Single_Period;
       Next_Sample_Start : Time := System_Start;
       Sine_Sample : Float := 0.0;
+      Sine_X : Float := 0.0;
    begin
       loop
-         On (Red);
-         -- Loop for detect time.
+         -- Reset the sine x.
+         Sine_X := 0.0;
          for Detect_Time in Sample_Range loop
             Next_Sample_Start := Next_Sample_Start + Calc_Signal_Period;
             -- Calculate the sample.
-            Sine_Sample := Sine (Sample_X_Slide);
-            -- Update the light.
---              if Sine_Sample > 0.0 then
---                 On (Blue);
---              else
---                 Off (Blue);
---              end if;
+            Sine_Sample := Sine (Sine_X);
+            Sine_X := Sine_X + Sample_X_Slide;
+            -- Update the light, write the signal.
+            if Sine_Sample < 0.0 then
+               On (Blue);
+               for Port in Com_Ports'First .. Com_Ports'Last loop
+                  Reset  (Port);
+                  Off ((Port, R));
+               end loop;
+
+            else
+               Off (Blue);
+               for Port in Com_Ports'First .. Com_Ports'Last loop
+                  Set  (Port);
+                  On ((Port, R));
+               end loop;
+            end if;
             -- Time limitation for one single sample.
             delay until Next_Sample_Start;
          end loop;
-         -- Time limitation for one single period.
-         delay until Next_Sine_Start;
       end loop;
    end Signal_Generator;
 
